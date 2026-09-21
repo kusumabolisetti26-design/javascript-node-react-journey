7291c829a4463b58f70e2664fe2bb9ac6e240ae7
kusumabolisetti26-design <kusumabolisetti26@gmail.com>
2026-09-21 21:15:54 +0530

    Day 1: JavaScript basics

---
+++ day1.js
@@ -0,0 +1,28 @@
+let name="kusuma";
+let age=20;
+let clg="vishnu";
+let cgpa=9.12;
+let isStudent=true;
+console.log(name);
+console.log(age);
+console.log(clg);
+console.log(cgpa);
+console.log(isStudent);
+
+let a = 25;
+let b = 15;
+let sum=a+b;
+let diff=a-b;
+let pro=a*b;
+console.log("sum of a and b is",sum);
+console.log("difference of a and b is",diff);
+console.log("product of a and b is",pro);
+
+let stu="jeshuu";
+let old=20;
+let branch="IT";
+let sgpa=9.12;
+console.log("my name is",stu);
+console.log("i am ",old,"age");
+console.log("I am studying",branch);
+console.log("My SGPA is",sgpa)
