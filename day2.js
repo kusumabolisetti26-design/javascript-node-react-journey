99c2a20dde327b09236a88109460451cd2423448
kusumabolisetti26-design <kusumabolisetti26@gmail.com>
2026-09-22 21:11:40 +0530

    Day 2: JavaScript conditions

---
+++ day2.js
@@ -0,0 +1,48 @@
+let number=17;
+if(number%2==0)
+{
+    console.log("Even");
+}
+else
+{
+    console.log("Odd");
+}
+
+let marks = 65;
+if(marks>40)
+{
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+let markss = 82;
+if(markss>=90)
+{
+    console.log("A");
+}
+else if(markss>=75&&markss<=89)
+{
+    console.log("B");
+}
+else if(markss>=60&&markss<=74)
+{
+    console.log("C");
+}
+else if(markss>=40&&markss<=59)
+{
+    console.log("D");
+}
+else{
+    console.log("Fail");
+}
+
+let username = "admin";
+let password = "12345";
+if(username==="admin"&&password==="1234"){
+        console.log("Login successful");
+    }
+else{
+console.log("Invalid username or password");
+}
