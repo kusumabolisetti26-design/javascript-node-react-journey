6aef507a47588f3240aa385f596d65acf135b867
kusumabolisetti26-design <kusumabolisetti26@gmail.com>
2026-09-23 18:53:29 +0530

    Day 3: JavaScript loops

---
+++ day3.js
@@ -0,0 +1,33 @@
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+
+for(let j=1;j<=20;j++)
+{
+    if(j%2==0)
+    {
+    console.log(j);
+    }
+}
+
+for(let j=1;j<=20;j++)
+{
+    if(j%2!=0)
+    {
+    console.log(j);
+    }
+}
+
+let sum=0;
+for(let i=1;i<=10;i++)
+{
+    sum=sum+i;
+}
+console.log(sum);
+
+let num=7;
+for(let i=1;i<=10;i++)
+{
+    console.log(num,"*",i,"=",num*i);
+}
